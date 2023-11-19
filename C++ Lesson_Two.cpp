f3d7e64a177332d69454af396fb7f823611e427b
Sarie89 <150705779+Sarie89@users.noreply.github.com>
2023-11-18 19:28:51 -0500

    Add files via upload

---
+++ C++ Lesson_Two.cpp
@@ -0,0 +1,68 @@
+/*
+DATA TYPES:
+    - boolean:Stores true or false values
+    - char: Stores a single character/letter/number, or ASCII values
+    - int: Stores whole numbers, without decimals
+    - double: Stores fractional numbers, containing one or more decimals. 
+    - string: Stores a sequence of characters
+
+  Numbers:
+    - Use int when you need to store a whole number without decimals, like 35 or 1000
+    - Double when you need a floating point number (with decimals), like 9.99 or 3.14515.
+    - A floating point number can also be a scientific number with an "e" to indicate the power of 10. EX: f1 = 35e3
+
+  Boolean:
+    - A boolean data type is declared with the bool keyword and can only take the values true or false.
+    - When the value is returned, true = 1 and false = 0.
+    - You can use a comparison operator, such as the greater than (>) operator, to find out if an expression (or variable) is true or false
+    - cout << (10 > 9); // returns 1 (true), because 10 is higher than 9
+  
+  Text:
+    - The string type is used to store a sequence of characters (text). 
+    - This is not a built-in type, but it behaves like one in its most basic usage. String values must be surrounded by double quotes
+    - The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c'
+
+OPERATORS:
+- Operators are used to perform operations on variables and values.
+  
+  Arithmetic: 
+    +	Addition. Adds together two values.	x + y	
+    -	Subtraction. Subtracts one value from another.	x - y	
+    *	Multiplication.	Multiplies two values.	x * y	
+    /	Division. Divides one value by another.	x / y	
+    %	Modulus. Returns the division remainder. x % y	
+    ++	Increment.	Increases the value of a variable by 1.	++x	
+    --	Decrement.	Decreases the value of a variable by 1.	--x
+
+  Comparison:
+    ==	Equal to	x == y	
+    !=	Not equal	x != y	
+    >	Greater than	x > y	
+    <	Less than	x < y	
+    >=	Greater than or equal to	x >= y	
+    <=	Less than or equal to	x <= y
+
+  Logical:
+    && 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+    || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+    !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+*/
+
+// EXCERCISE:
+
+// 1. Convert this equation into C++ code (7 – 12) / (6 – 1)
+
+#include <iostream>
+
+int main(){
+
+  
+}
+
+
+// 2. Find the area of a triangle with the measerments of base = 3 and height = 4 and execute the anwser
+
+
+
+// 3. Create a application that calculates and displays the average jump length of an athlete whose jumps were 3.3 m. 4.0 m and 3.0 m.
+
