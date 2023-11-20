4121120a8f1665c5c25d233993f97947b673f889
Sarie89 <150705779+Sarie89@users.noreply.github.com>
2023-11-20 18:54:50 -0500

    Create C++ Lesson_Five.cpp

---
+++ C++ Lesson_Five.cpp
@@ -0,0 +1,18 @@
+LOOPS:
+  - A loop is a programming tool that repeats some code or a set of instructions until a specified condition is reached.
+  - You’ll hear the generic term “iterate” when referring to loops; iterate simply means “to repeat”
+
+WHILE LOOPS:
+  - The While loop will continue to execute the code inside of it, over and over again, as long as the condition is true.
+  EX:
+   1.  while (condition) {
+
+    statements
+}
+  2. while (guess != 8) {
+  
+      std::cout << "Wrong guess, try again: ";
+      std::cin >> guess;
+    
+    }
+
