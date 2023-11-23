c3a0c8bd80f48ae2cccc5e2656558ccbe035387f
Sarie89 <150705779+Sarie89@users.noreply.github.com>
2023-11-22 19:51:07 -0500

    Update C++ Lesson_Six.cpp

--- C++ Lesson_Six.cpp
+++ C++ Lesson_Six.cpp
@@ -98,3 +98,89 @@ LOOP THROUGH ARRAYS:
         cout << i << "\n";
       }
 
+MULTI-DIMENSTIONAL ARRAYS:
+  - A multi-dimensional array is an array of arrays.
+  - To declare a multi-dimensional array, define the variable type, specify the name of the array followed by square brackets which specify 
+  - how many elements the main array has, followed by another set of square brackets which indicates how many elements the sub-arrays have:
+  EX:
+      string letters[2][4];
+  - As with ordinary arrays, you can insert values with an array literal - a comma-separated list inside curly braces. 
+  - In a multi-dimensional array, each element in an array literal is another array literal.
+  EX:
+      string letters[2][4] = {
+        { "A", "B", "C", "D" },
+        { "E", "F", "G", "H" }
+      };
+  - Each set of square brackets in an array declaration adds another dimension to an array. 
+  - An array like the one above is said to have two dimensions.
+  - Arrays can have any number of dimensions. The more dimensions an array has, the more complex the code becomes. 
+    The following array has three dimensions:
+  EX:
+      string letters[2][2][2] = {
+          {
+            { "A", "B" },
+            { "C", "D" }
+          },
+          {
+            { "E", "F" },
+            { "G", "H" }
+          }
+        };
+ACCESSING ELEMENTS OF MULTI-DIMENSIONAL ARRAYS:
+  - To access an element of a multi-dimensional array, specify an index number in each of the array's dimensions.
+  EX:
+      string letters[2][4] = {
+      { "A", "B", "C", "D" },
+      { "E", "F", "G", "H" }
+    };
+    
+    cout << letters[0][2];  // Outputs "C"
+
+CHANGING ELEMENTS OF MULTI_DIMENTIONAL ARRAYS:
+  - To change the value of an element, refer to the index number of the element in each of the dimensions:
+  EX:
+      string letters[2][4] = {
+        { "A", "B", "C", "D" },
+        { "E", "F", "G", "H" }
+      };
+      letters[0][0] = "Z";
+      
+      cout << letters[0][0];  // Now outputs "Z" instead of "A"
+
+LOOPING THROUGH A MULTI_DIMENSIONAL ARRAY:
+  - To loop through a multi-dimensional array, you need one loop for each of the array's dimensions.
+  EX: 
+      string letters[2][4] = {
+        { "A", "B", "C", "D" },
+        { "E", "F", "G", "H" }
+      };
+      
+      for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+          cout << letters[i][j] << "\n";
+        }
+      }
+  For a three dimentional array:
+      string letters[2][2][2] = {
+      {
+        { "A", "B" },
+        { "C", "D" }
+      },
+      {
+        { "E", "F" },
+        { "G", "H" }
+      }
+    };
+    
+    for (int i = 0; i < 2; i++) {
+      for (int j = 0; j < 2; j++) {
+        for (int k = 0; k < 2; k++) {
+          cout << letters[i][j][k] << "\n";
+        }
+      }
+    }
+
+
+
+
+  
